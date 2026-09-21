8a5bd798cf286d50953331d99b54056cd5910bf9
vk698 <vishalkumar2257r@gmail.com>
2026-09-21 16:07:01 +0530

    commit

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -51,11 +51,12 @@ export const login = async (req, res) => {
     }
 
     const token = await createJwtToken(user._id, process.env.JWT_SECRET);
-
+ 
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "lax",
+      domain:process.env.COOKIE_DOMAIN,
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     });
 
