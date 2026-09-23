ffa3a7deb4f586caa5321ef2b3e256d9d0cabb4a
vk698 <vishalkumar2257r@gmail.com>
2026-09-23 15:36:01 +0530

    Update cookie settings for registration to enhance security and consistency

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -25,10 +25,9 @@ export const register = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      domain: process.env.COOKIE_DOMAIN,
-      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      secure: true,
+      sameSite: "none",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(201).json({ message: "User registered successfully", token });
